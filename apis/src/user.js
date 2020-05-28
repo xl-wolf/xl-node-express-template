5634625b7d59a7988e679dd2753b82f923d3be53
xl-wolf <1072236984@qq.com>
2020-05-28 10:08:52 +0800

    登录登出接口

--- apis/src/user.js
+++ apis/src/user.js
@@ -40,21 +40,49 @@ router.post("/user/register", (req, res) => {
 
 // 登录
 router.post("/user/login", (req, res) => {
-  let { uname, upwd } = req.body;
-  let sql = `SELECT * FROM user_info WHERE uname = '${uname}' AND upwd = '${upwd}'`;
-  DB.sqlExcutor(sql)
-    .then((resData) => {
-      console.log(resData);
-      let { fields, results } = resData;
-      if (results.length > 0) {
-        res.json({ status: "200", msg: "登录成功" });
-      } else {
-        res.json({ status: "500", msg: "用户名或密码错误" });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  console.log("req.body", req);
+  const { username, password } = req.query;
+  let resData = null;
+  switch (username) {
+    case "admin":
+      resData = {
+        token: "admin",
+        role: "admin",
+        projectName: "xl-wolf",
+        avatar: "https://avatars0.githubusercontent.com/u/45027348?s=60&v=4",
+      };
+      break;
+    case "guest":
+      resData = {
+        token: "guest",
+        role: "guest",
+        projectName: "guest",
+        avatar: "https://avatars0.githubusercontent.com/u/45027348?s=60&v=4",
+      };
+      break;
+    default:
+      break;
+  }
+  res.json({ status: "200", msg: "登录成功", data: resData });
+  // const sql = `SELECT * FROM user_info WHERE uname = '${uname}' AND upwd = '${upwd}'`;
+  // DB.sqlExcutor(sql)
+  //   .then((resData) => {
+  //     console.log(resData);
+  //     let { fields, results } = resData;
+  //     if (results.length > 0) {
+  //       res.json({ status: "200", msg: "登录成功" });
+  //     } else {
+  //       res.json({ status: "500", msg: "用户名或密码错误" });
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+});
+
+// 登出
+router.post("/user/logout", (req, res) => {
+  res.json({ status: "200", msg: "登出成功" });
 });
 
 module.exports = router;
