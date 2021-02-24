87b109db7c6414b0a2f042e88a6fa1f095d3c817
xl-wolf <1072236984@qq.com>
2021-02-24 11:37:03 +0800

    简化无用代码与配置文件

--- dbConnection/databasePool.js
+++ dbConnection/databasePool.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
-const serverUrl = require("./serverUrl");
-const user = require("./user");
+const { host } = require("./serverUrl");
+const { user, password } = require("./user");
 const connectionPool = mysql.createPool({
-  host: serverUrl.host, //数据库所在服务器地址
-  user: user.user, //用户名
-  password: user.password, //密码
+  host: host, //数据库所在服务器地址
+  user: user, //用户名
+  password: password, //密码
   database: "test", //数据库名
   port: 3306, //数据库端口
 });
